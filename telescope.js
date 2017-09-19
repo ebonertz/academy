368dc6be536f8afd5780177a0a7a65c4c36167aa
Evan Bonertz <evan.bonertz@commercetools.com>
2017-09-19 16:21:46 -0400

    Add starcount for loop

--- telescope.js
+++ telescope.js
@@ -9,3 +9,13 @@ const visibleLightWaves = () => {
   };
 
 visibleLightWaves();
+
+const starCount = () => {
+  let i = 5;
+  console.log(i);
+  for (let i = 0; i < 12; i++) {
+    console.log(i);
+  }
+};
+
+starCount();
