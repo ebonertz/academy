99a9f98808923c49c42713aa29f2920673c022ad
Evan Bonertz <evan.bonertz@commercetools.com>
2017-11-09 16:34:35 -0500

    add arrays exercise

---
+++ resoliionary.js
@@ -0,0 +1,8 @@
+let newYearsResolutions = ["Get Chiseled","Stop Smoking","Eat Right"]
+let listItem = "Get Chiseled"
+newYearsResolutions[1] = "Learn a new language"
+newYearsResolutions.push("Spend Less", "Save More")
+newYearsResolutions.pop();
+console.log(newYearsResolutions);
+console.log(listItem);
+console.log(newYearsResolutions.length);
