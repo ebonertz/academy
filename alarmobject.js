fe5c9508e7fa644534a665ad5b94700daf907706
Evan Bonertz <evan.bonertz@commercetools.com>
2017-12-05 16:58:54 -0500

    Update objects with methods

--- alarmobject.js
+++ alarmobject.js
@@ -2,7 +2,15 @@ let person = {
   name: 'Evan',
   age: 28,
   weekendAlarm: 'No alarms needed',
-  weekAlarm: 'Alarm set to 7AM'
+  weekAlarm: 'Alarm set to 7AM',
+  //Add a method with ES6 syntax
+  sayGoodbye() {
+    return 'Goodbye!'
+  },
+  //Add a method that returns name from within the same object using this Keyword
+  sayHello() {
+    return `Hello, my name is ${this.name}`
+  }
 };
 
 console.log(person['name']);
@@ -18,3 +26,14 @@ if (day === 'Saturday' || day === 'Sunday' ) {
 }
 
 console.log(person[alarm]);
+
+// Add new properties with dot notation
+person.hobbies = ['snowboarding', 'bass']
+console.log(person.hobbies);
+
+//Add new method with arrow function syntax that returns a string
+person.sayHello = () => {
+  return 'Hello, there!'
+}
+
+console.log(person.sayHello());
