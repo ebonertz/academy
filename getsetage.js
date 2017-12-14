581d773f26d4c37a355d377af9ff4ca215feba2f
Evan Bonertz <evan.bonertz@commercetools.com>
2017-12-14 15:49:21 -0500

    Add age with get and set

---
+++ getsetage.js
@@ -0,0 +1,26 @@
+let person = {
+  _name: 'Evan',
+  _age: 28,
+
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
+  },
+
+  get age() {
+    console.log(`${this._name} is ${this._age} years old.`);
+    return this._age;
+  }
+
+};
+
+
+//person.age = 'Thirty-nine';
+person.age = 39;
+
+console.log(person.age);
